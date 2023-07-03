67165dd46ffc2ba1c6b885db4d3c18424993052e
lifey <lifey@cast.cn>
2023-07-03 15:08:34 +0800

    使用api以及apipost测试

---
+++ day01/code-API/03_记账本/models/BookModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+//创建文档的结构对象
+//设置集合中文档的属性以及属性值的类型
+let Bookschema = new mongoose.Schema({
+    name: String,
+    author: String,
+    price: Number
+})
+
+//创建模型对象 对文档操作的封装对象
+let BookModel = mongoose.model('books', Bookschema)
+
+module.exports = BookModel;
